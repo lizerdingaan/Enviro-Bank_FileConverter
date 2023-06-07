940ffba26ad537cf1e7878b112864ccd23a7402b
Tshegofatso Dingaan <tshdinga021@student.wethinkcode.co.za>
2023-06-07 22:30:27 +0200

    spring boot skeleton

---
+++ src/main/java/com/enviro/assessment/grad001/tshegofatsodingaan/EnviroBank/EnviroBankApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.tshegofatsodingaan.EnviroBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviroBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviroBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/tshegofatsodingaan/EnviroBank/EnviroBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.tshegofatsodingaan.EnviroBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
