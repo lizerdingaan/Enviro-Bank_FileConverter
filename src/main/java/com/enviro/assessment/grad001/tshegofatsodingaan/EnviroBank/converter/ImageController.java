44e930fa080a762116df7d53522131ed659339db
tshego <tshdinga021@student.wethinkcode.co.za>
2023-06-09 20:31:22 +0200

    implemented the controller, uncommented the jpa dependencies and implemented the database entities

--- src/main/java/com/enviro/assessment/grad001/tshegofatsodingaan/EnviroBank/converter/ImageController.java
+++ src/main/java/com/enviro/assessment/grad001/tshegofatsodingaan/EnviroBank/converter/ImageController.java
@@ -1,6 +1,7 @@
 package com.enviro.assessment.grad001.tshegofatsodingaan.EnviroBank.converter;
 
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/api/image")
 public class ImageController {
 
+    @GetMapping(value = "/{name}/{surname}")
     public void gethttpImageLink(@PathVariable String name, @PathVariable String surname){
 
     }
